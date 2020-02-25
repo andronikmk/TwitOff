90f40fb61942e996cade1ea2711608679f5301d8
andronikmk <andronikmk@gmail.com>
2020-02-24 20:53:31 -0500

    finished first part of assignment

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,15 +1,18 @@
 """Main application and routing logic for TwitOff"""
-from flask import Flask
-from .models import DB
+from flask import Flask, render_template, request
+from .models import DB, User
 
 def create_app():
     """Create and configure an instance of the Flask applications."""
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['ENV'] = 'debug' # TODO: change before deploying
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
     @app.route("/")
     def root():
-        return "Welcome to TwitOff!, First Flask APP!!!"
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
    
     return app
