38ba77b3d6b7639867b0881d96b700ba110037cb
andronikmk <andronikmk@gmail.com>
2020-02-25 19:16:45 -0500

    add_or_update_user method

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,12 +2,14 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .twitter import add_or_update_user
 
 def create_app():
     """Create and configure an instance of the Flask applications."""
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
-    app.config['ENV'] = config('ENV')
+    # app.config['ENV'] = config('ENV')
+    
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
@@ -16,6 +18,25 @@ def create_app():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
     
+    @app.route("/user", methods=['POST'])
+    @app.route("/user/<name>", methods=['GET'])
+    def user(name=None):
+        message = ''
+        # import pdb: pdb.set_trace()
+        # name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = 'User {} successfullt added!'.format(name)
+            tweets = User.filter(User.name == name).one().tweets
+            pass
+        except Exception as e:
+            message = 'Error adding {}: {}'.format(name, e)
+            tweets = []
+            pass
+        return render_template('user.html', title=name, tweets=tweets,
+                                             message=message)
+
     @app.route("/reset")
     def reset():
         DB.drop_all()
